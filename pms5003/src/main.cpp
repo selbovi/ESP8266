ce6e3d9a0d8cafe45e14092b3858beb89bf3e611
Салават Муфаззалов <s.mufazzalov@soccard.ru>
2024-04-07 02:37:14 +0300

    read serial

--- pms5003/src/main.cpp
+++ pms5003/src/main.cpp
@@ -2,21 +2,37 @@
 #include <SoftwareSerial.h>
 #endif
 #include <PMserial.h> // Arduino library for PM sensors with serial interface
-
+#include <Stream.h>
 
 void setup()
 {
-  Serial.begin(9600);
-
+	Serial.begin(9600);
 }
 
 void loop()
 {
- while (Serial.available())
- {
-	int ch = Serial.read();
-	Serial.println(ch, HEX);
-	delay(300);
- }
- 
+
+	while (Serial.available())
+	{
+		int ch = Serial.read();
+		Serial.println(ch, HEX);
+		if (Serial.find(66))
+		{ // 0x42
+			Serial.println("found 0x42");
+			ch = Serial.read();
+			Serial.println(ch);
+			if (ch == 77)
+			{ // 0x4d
+
+				for (int i = 0; i < 30; i++)
+				{
+					ch = Serial.read();
+					Serial.println(ch);
+				}
+			}
+		}
+		Serial.println(" ========================================== ");
+		return;
+	}
+	delay(5000);
 }
