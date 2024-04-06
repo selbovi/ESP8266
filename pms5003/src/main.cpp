7f389591defed36ccc0fe984579ce4eb88cfc1c5
Салават Муфаззалов <s.mufazzalov@soccard.ru>
2024-04-07 02:53:26 +0300

    read with lib

--- pms5003/src/main.cpp
+++ pms5003/src/main.cpp
@@ -4,35 +4,66 @@
 #include <PMserial.h> // Arduino library for PM sensors with serial interface
 #include <Stream.h>
 
+#define MSG "PMSx003 on HardwareSerial"
+
+SerialPM pms(PMSx003, Serial); // PMSx003, UART
+
 void setup()
 {
 	Serial.begin(9600);
+
+	Serial.println(F(MSG));
+	pms.init();
 }
 
 void loop()
 {
 
-	while (Serial.available())
-	{
-		int ch = Serial.read();
-		Serial.println(ch, HEX);
-		if (Serial.find(66))
-		{ // 0x42
-			Serial.println("found 0x42");
-			ch = Serial.read();
-			Serial.println(ch);
-			if (ch == 77)
-			{ // 0x4d
+	// read the PM sensor
+	pms.read();
+	if (pms)
+	{ // successfull read
+	  // print formatted results
+		Serial.printf("PM1.0 %2d, PM2.5 %2d, PM10 %2d [ug/m3]\n",
+					  pms.pm01, pms.pm25, pms.pm10);
 
-				for (int i = 0; i < 30; i++)
-				{
-					ch = Serial.read();
-					Serial.println(ch);
-				}
-			}
-		}
-		Serial.println(" ========================================== ");
-		return;
+		if (pms.has_temperature_humidity() || pms.has_formaldehyde())
+			Serial.printf("%5.1f °C, %5.1f %%rh, %5.2f mg/m3 HCHO\n",
+						  pms.temp, pms.rhum, pms.hcho);
 	}
-	delay(5000);
+	else
+	{ // something went wrong
+		switch (pms.status)
+		{
+		case pms.OK: // should never come here
+			break;	 // included to compile without warnings
+		case pms.ERROR_TIMEOUT:
+			Serial.println(F(PMS_ERROR_TIMEOUT));
+			break;
+		case pms.ERROR_MSG_UNKNOWN:
+			Serial.println(F(PMS_ERROR_MSG_UNKNOWN));
+			break;
+		case pms.ERROR_MSG_HEADER:
+			Serial.println(F(PMS_ERROR_MSG_HEADER));
+			break;
+		case pms.ERROR_MSG_BODY:
+			Serial.println(F(PMS_ERROR_MSG_BODY));
+			break;
+		case pms.ERROR_MSG_START:
+			Serial.println(F(PMS_ERROR_MSG_START));
+			break;
+		case pms.ERROR_MSG_LENGTH:
+			Serial.println(F(PMS_ERROR_MSG_LENGTH));
+			break;
+		case pms.ERROR_MSG_CKSUM:
+			Serial.println(F(PMS_ERROR_MSG_CKSUM));
+			break;
+		case pms.ERROR_PMS_TYPE:
+			Serial.println(F(PMS_ERROR_PMS_TYPE));
+			break;
+		}
+	}
+
+	// wait for 10 seconds
+	delay(10000);
 }
